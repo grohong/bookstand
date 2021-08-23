9a8eb8ba3df355532b8f726f309a8fe0155bc6b2
홍성호 <sh.hong@woowahan.com>
2021-08-24 00:05:10 +0900

    init

---
+++ bookstand/Entry/SceneDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  SceneDelegate.swift
+//  bookstand
+//
+//  Created by sh.hong on 2021/08/23.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: scene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
+
---
+++ bookstand/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  bookstand
+//
+//  Created by sh.hong on 2021/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
